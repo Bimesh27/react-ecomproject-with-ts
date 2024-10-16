7759b3c7b6abe66ab1ee753e77eff659ff4bebc6
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-10-16 19:31:52 +0530

    add products and filter for product

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import {BrowserRouter as Router} from "react-router-dom"
-import Sidebar from "./components/Sidebar"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
 
 const App = () => {
   return (
     <Router>
       <div className="flex h-screen">
-        <Sidebar/>
+        <Sidebar />
+
+        <div className="rounded w-full flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+          </Routes>
+        </div>
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
