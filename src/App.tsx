0c94223b5f1367977f6601bf2617eaeea53b050d
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-10-17 09:23:23 +0530

    add productpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
 
 const App = () => {
   return (
     <Router>
       <div className="flex h-screen">
         <Sidebar />
-
         <div className="rounded w-full flex justify-between flex-wrap">
           <Routes>
             <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
         </div>
       </div>
